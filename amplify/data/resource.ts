aa39481c98d048f8762ab60db61019ce63189cc6
nikolaPetkovic96 <44683836+nikolaPetkovic96@users.noreply.github.com>
2026-02-07 18:59:45 +0100

    added per-user data isolation-2

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -21,7 +21,7 @@ export const data = defineData({
   schema,
   authorizationModes: {
     //defaultAuthorizationMode: "apiKey",
-    defaultAuthorizationMode: "userPool",
+    defaultAuthorizationMode: 'userPool',
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
